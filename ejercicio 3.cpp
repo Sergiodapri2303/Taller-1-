b955d506256e5de85e070303fd83f4e813ce1dc3
Sergiodapri2303 <sergio.prieto03@usa.edu.co>
2025-02-13 08:32:36 -0500

    Create ejercicio 3.cpp

---
+++ ejercicio 3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 10;
+  int y = 5;
+  int z = x + y;
+  cout << z << endl;
+  
+  int a = z - 3;
+  cout << a << endl;
+
+  int b = a * 2;
+  cout << b << endl;
+
+  int c = b / 4;
+  cout << c << endl;
+  
+  return 0;
+}
