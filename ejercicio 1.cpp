abb43c0f3b6c28c259857dd75ff6da7f2f3ece38
Sergiodapri2303 <sergio.prieto03@usa.edu.co>
2025-02-13 08:30:34 -0500

    Create ejercicio 1.cpp

---
+++ ejercicio 1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 5;
+  int y = 10;
+  int sum = x + y;
+  int resta = x - y;
+  int multiplicacion = x * y;
+  int division = x / y;
+  int modulo = x % y;
+  cout << "Suma: " << sum << endl;
+  cout << "Resta: " << resta << endl;
+  cout << "Multiplicacion: " << multiplicacion << endl;
+  cout << "Division: " << division << endl;
+  cout << "Modulo: " << modulo << endl;
+  return 0;
+}
